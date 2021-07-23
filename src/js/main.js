f44c7a074586586d5f1730fb7f38347ab2991dda
RB <bobrovskiyroman@gmail.com>
2021-07-23 09:23:52 +0300

    add input

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,48 @@
 import inputSearch from "../tempaltes/search.hbs";
 class Gallery {
-  constructor(data) {
-    this.baseUrl = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=`;
+  constructor() {
     this.search = "";
     this.pageNumber = 1;
     this.apiKey = "22602926-3614d0f9783b43324aa2f82a8";
-    this.URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.search}&page=${this.pageNumber}&per_page=12&key=${this.apiKey}`;
     this.list = document.querySelector("#gallery");
     this.divInputSearch = document.querySelector("#inputSearch");
-    this.data = data;
+    this.fetchImages();
+    console.log(this);
   }
-
   fetchImages() {
-    fetch(this.URL)
+    let URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.search}&page=${this.pageNumber}&per_page=12&key=${this.apiKey}`;
+    console.log(URL);
+    console.log(this);
+    fetch(URL)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data.hits);
-        // console.log(this.URL);
         this.publuc(data.hits);
       })
       .catch((err) => console.log(err));
   }
+
   publuc = (arrData) => {
-    // console.log(this);
     this.divInputSearch.innerHTML = inputSearch();
+    this.search = document.querySelector("#search-form")[0];
+    this.search.addEventListener(
+      "input",
+      _.debounce(this.getInputData.bind(this), 500)
+    );
     arrData.map((obj) => {
-      console.log(obj);
       this.list.insertAdjacentHTML(
         "beforeend",
         `<li class="smallImages"> <img src="${obj.previewURL}" alt="${obj.tags}"></li>`
       );
     });
   };
+
+  getInputData() {
+    this.search = document.querySelector("#search-form")[0].value;
+    let result = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.search}&page=${this.pageNumber}&per_page=12&key=${this.apiKey}`;
+    this.fetchImages(result);
+    return result;
+  }
 }
-new Gallery().fetchImages();
+
+let _ = require("lodash");
+new Gallery();
