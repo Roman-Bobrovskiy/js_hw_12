d5f3543fcdba4a1540120cb08a9ad067e411b863
RB <bobrovskiyroman@gmail.com>
2021-07-23 16:39:17 +0300

    btn

--- src/js/main.js
+++ src/js/main.js
@@ -3,16 +3,19 @@ import cards from "../tempaltes/cards.hbs";
 class Gallery {
   constructor() {
     this.search = "";
-    this.pageNumber = 1;
+    this.page = 1;
+    this.perPage = 12;
     this.apiKey = "22602926-3614d0f9783b43324aa2f82a8";
     this.list = document.querySelector("#gallery");
     this.divInputSearch = document.querySelector("#inputSearch");
+    this.btnLoadMore = document.querySelector(".btnLoadMore");
     this.fetchImages();
+    this.loadMoreBtn();
     console.log(this);
   }
 
   fetchImages() {
-    let URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.search}&page=${this.pageNumber}&per_page=12&key=${this.apiKey}`;
+    let URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.search}&page=${this.page}&per_page=${this.perPage}&key=${this.apiKey}`;
     console.log(URL);
     console.log(this);
     fetch(URL)
@@ -61,13 +64,21 @@ class Gallery {
 
   getInputData() {
     this.search = document.querySelector("#search-form")[0].value;
-    let result = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.search}&page=${this.pageNumber}&per_page=12&key=${this.apiKey}`;
+    let result = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.search}&page=${this.page}&per_page=${this.perPage}&key=${this.apiKey}`;
     this.fetchImages(result);
     return result;
   }
 
   removeData() {
     this.list.innerHTML = "";
+  }
+  loadMoreBtn() {
+    this.btnLoadMore.addEventListener("click", () => {
+      this.perPage = this.perPage += this.perPage;
+      console.log(this.search.value);
+      this.getInputData(this.perPage);
+      return this.perPage;
+    });
   }
 }
 
